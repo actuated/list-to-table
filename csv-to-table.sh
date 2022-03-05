0d9247e51d66c0de69004014ce3a516974fb04f6
actuated <actuated@users.noreply.github.com>
2022-03-04 20:20:27 -0600

    Added column fill logic to csv-to-table

--- csv-to-table.sh
+++ csv-to-table.sh
@@ -38,7 +38,28 @@ fi
 
 # Open/close HTML doc and sed CSV lines to table rows
 echo "<html><body><table>" >> "$outHtmlFile"
-sed 's/^/<tr><td>/g' "$sourceFile" | sed 's/,/<\/td><td>/g' | sed 's/$/<\/td><\/tr>/g' >> "$outHtmlFile"
+# Check maximum number of CSV columns
+maxCols=$(awk -F , '{print NF}' "$sourceFile" | sort | tail -n 1)
+# Check each line
+while read thisLine; do
+  # Get this line's column count and any necessary fill count
+  thisCols=$(echo "$thisLine" | awk -F , '{print NF}')
+  fillCount=$(( "$maxCols" - "$thisCols" ))
+  # Start row
+  echo "<tr><td>" >> "$outHtmlFile"
+  # Replace commas with cell close/open tags
+  echo "$thisLine" | sed 's/,/<\/td><td>/g' >> "$outHtmlFile"
+  # Check for fill count
+  if [ "$fillCount" -gt "0" ]; then
+    countFillCols=0
+    while [ "$countFillCols" -lt "$fillCount" ]; do
+      echo "</td><td>"  >> "$outHtmlFile"
+      countFillCols=$(( $countFillCols + 1 ))
+    done
+  fi
+  # End row
+  echo "</td></tr>" >> "$outHtmlFile"
+done < "$sourceFile"
 echo "</table></body></html>" >> "$outHtmlFile"
 
 # Prompt to open document
